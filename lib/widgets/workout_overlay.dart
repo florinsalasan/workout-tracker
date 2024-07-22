57ea280070a938451c56bee8b7003c624c6ad04c
Florin Salasan <florinsalasan@gmail.com>
2024-07-22 01:42:57 -0400

    changed workoutOverlay to use DraggableScrollableSheet instead of the janky GestureDetector and Container mixes, when minimized the tabbar is not visible so that still needs to be fixed, but everything else seems to be working

--- lib/widgets/workout_overlay.dart
+++ lib/widgets/workout_overlay.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:workout_tracker/models/workout_model.dart';
@@ -184,139 +185,107 @@ class WorkoutState extends ChangeNotifier {
 class WorkoutOverlay extends StatelessWidget {
   const WorkoutOverlay({super.key});
 
-  void _handleDrag(BuildContext context, DragUpdateDetails details) {
-    final workoutState = context.read<WorkoutState>();
-    final newHeight = (workoutState.overlayHeight - details.delta.dy)
-        .clamp(WorkoutState.minHeight, WorkoutState.maxHeight);
-    workoutState.updateOverlayHeight(newHeight);
-  }
-
-  void _handleDragEnd(BuildContext context, DragEndDetails details) {
-    final workoutState = context.read<WorkoutState>();
-    workoutState.snapOverlay();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutState>(
       builder: (context, workoutState, child) {
-        return GestureDetector(
-          onVerticalDragUpdate: (details) => _handleDrag(context, details),
-          onVerticalDragEnd: (details) => _handleDragEnd(context, details),
-          child: Container(
-            height: workoutState._overlayHeight,
-            decoration: BoxDecoration(
-                // color: CupertinoColors.systemGreen.withOpacity(0.9),
-                color: CupertinoColors.systemBackground,
+        return DraggableScrollableSheet(
+          builder: (BuildContext context, ScrollController scrollController) {
+            return Container(
+              decoration: BoxDecoration(
+                color: CupertinoColors.white,
                 borderRadius:
                     const BorderRadius.vertical(top: Radius.circular(20)),
                 boxShadow: [
                   BoxShadow(
-                      color: CupertinoColors.black.withOpacity(0.1),
-                      blurRadius: 10,
-                      offset: const Offset(0, -5))
-                ]),
-            child: Column(
-              children: [
-                _buildHandle(context, workoutState),
-                Expanded(
-                  child: CustomScrollView(
-                    slivers: _buildSlivers(context, workoutState),
+                    color: CupertinoColors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: const Offset(0, -5),
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-              ],
-            ),
-          ),
+                ],
+              ),
+              child: CustomScrollView(
+                controller: scrollController,
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: _buildHandle(context, workoutState),
+                  ),
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        "Active Workout",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ),
+                  ),
+                  _buildExerciseList(workoutState),
+                  SliverToBoxAdapter(
+                      child: _buildAddExerciseButton(context, workoutState)),
+                  SliverToBoxAdapter(
+                      child: _buildCancelWorkoutButton(context, workoutState)),
+                  SliverToBoxAdapter(
+                      child: _buildEndWorkoutButton(context, workoutState)),
+                ],
+              ),
+            );
+          },
         );
       },
     );
   }
 
-  Widget _buildHandle(BuildContext context, WorkoutState workoutState) {
-    return Column(
-      children: [
-        Container(
-          height: 5,
-          width: 50,
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-              color: CupertinoColors.black,
-              borderRadius: BorderRadius.circular(100)),
-        ),
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  style: TextStyle(
-                      color: CupertinoColors.black,
-                      fontWeight: FontWeight.bold),
-                  "Active Workout",
-                ),
-              ],
+  Widget _buildAddExerciseButton(
+      BuildContext context, WorkoutState workoutState) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        child: const Text('Add Exercise'),
+        onPressed: () async {
+          final db = Provider.of<Database>(context, listen: false);
+          final result = await showCupertinoDialog<String>(
+            context: context,
+            builder: (dialogContext) => Provider<Database>.value(
+              value: db,
+              child: const ExerciseSelectionDialog(),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                _buildEndWorkoutButton(context, workoutState),
-              ],
-            ),
-          ],
-        ),
-      ],
+          );
+          if (result != null) {
+            if (!context.mounted) {
+              if (kDebugMode) print('did not mount');
+              Error();
+            } else {
+              workoutState.addExercise(result, context);
+            }
+          }
+        },
+      ),
     );
   }
 
-  List<Widget> _buildSlivers(BuildContext context, WorkoutState workoutState) {
-    return [
-      const SliverToBoxAdapter(
-        child: SizedBox(height: 20),
+  Widget _buildHandle(BuildContext context, WorkoutState workoutState) {
+    return Container(
+      width: 40,
+      height: 5,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: CupertinoColors.systemGrey3,
+        borderRadius: BorderRadius.circular(2.5),
       ),
-      ...workoutState.exercises.asMap().entries.map((entry) {
-        final index = entry.key;
-        final exercise = entry.value;
-        return SliverToBoxAdapter(
-          child: ExerciseTrackingWidget(
-            exerciseIndex: index,
-            exerciseName: exercise.name,
-          ),
-        );
-      }),
-      SliverToBoxAdapter(
-        child: CupertinoButton(
-          onPressed: () async {
-            final db = Provider.of<Database>(context, listen: false);
-            final result = await showCupertinoDialog<String>(
-              context: context,
-              builder: (dialogContext) => Provider<Database>.value(
-                value: db,
-                child: const ExerciseSelectionDialog(),
-              ),
-            );
-            if (result != null) {
-              if (!context.mounted) {
-                if (kDebugMode) {
-                  print(
-                      'context did not mount when adding a new exercise try again');
-                }
-                Error();
-              } else {
-                workoutState.addExercise(result, context);
-              }
-            }
-          },
-          child: const Text("Add Exercise"),
-        ),
+    );
+  }
+
+  Widget _buildExerciseList(WorkoutState workoutState) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final exercise = workoutState.exercises[index];
+          return ExerciseTrackingWidget(
+              exerciseName: exercise.name, exerciseIndex: index);
+        },
+        childCount: workoutState.exercises.length,
       ),
-      SliverToBoxAdapter(
-        child: _buildCancelWorkoutButton(context, workoutState),
-      ),
-    ];
+    );
   }
 
   _buildCancelWorkoutButton(BuildContext context, WorkoutState workoutState) {
